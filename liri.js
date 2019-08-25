6254816754327729b1472b566e7b4b56bf2f08f9
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-08-25 13:32:33 -0400

    updated liri.js with more vars

--- liri.js
+++ liri.js
@@ -1,13 +1,28 @@
 require("dotenv").config();
 
+var axios = require("axios");
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify)
-var bandsInTown = 
+var spotify = new Spotify(keys.spotify);
 
-spotify.search({ type: 'track', query: 'I Want it That Way' }, function(err, data) {
+//omdb -- movie-this//
+axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(function(response) {
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  });
+
+//bands in town -- concert-this//
+var artist = process.argv[3]
+axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function(response) {
+    console.log(response.data)
+})
+
+//spotify -- spotify-this-song//
+spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
    
   console.log(data); 
   });
+
+//random.txt -- do-what-it-says//
+
