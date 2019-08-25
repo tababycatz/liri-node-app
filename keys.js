8e8e42cf8cabc82d099e08e1a319addab058c722
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-08-25 12:23:11 -0400

    added client id and secret for spotify api

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.eb578643b2664d4796ff30a9432db6fd,
+  secret: process.env.af5834a708b143ebb73feffd19050702
 };
--- liri.js
+++ liri.js
@@ -1,3 +1,13 @@
 require("dotenv").config();
+
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify)
+var bandsInTown = 
+
+spotify.search({ type: 'track', query: 'I Want it That Way' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
