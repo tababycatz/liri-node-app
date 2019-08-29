a6103b029d3fe7f3d29cca734e81a9a7c23b3cf4
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-08-29 19:19:22 -0400

    updated movieThis function to default to Mr. Nobody

--- liri.js
+++ liri.js
@@ -18,30 +18,38 @@ var arg2 = process.argv[3];
 //Plot
 //Actors
 function movieThis(arg2) {
-    axios.get("http://www.omdbapi.com/?t=&plot=short&apikey=trilogy").then(function(response) {
-    console.log(
-    "Movie Title: " + response.data.Title, 
-    "\nYear: " + response.data.Year,
-    "\nIMDB Rating: " + response.data.imdbRating,
-    "\nRotten Tomatoes Rating: " + response.data.Ratings[1],
-    "\nCountry: " + response.data.Country,
-    "\nLanguage: " + response.data.Language,
-    "\nMovie Plot: " + response.data.Plot,
-    "\nActors: " + response.data.Actors);
-  });
-}
+    if (!arg2) {
+        arg2 = "Mr. Nobody"
+    }
+    console.log("arg2: " +arg2)
+    axios.get("http://www.omdbapi.com/?t=" + arg2 + "&plot=short&apikey=trilogy").then(function (response) {
+
+        // else {
+            console.log(
+                "Movie Title: " + response.data.Title,
+                "\nYear: " + response.data.Year,
+                "\nIMDB Rating: " + response.data.imdbRating,
+                "\nRotten Tomatoes Rating: " + response.data.Ratings[1],
+                "\nCountry: " + response.data.Country,
+                "\nLanguage: " + response.data.Language,
+                "\nMovie Plot: " + response.data.Plot,
+                "\nActors: " + response.data.Actors);
+        // }
+    })
+};
 
 //bands in town -- concert-this function//
 //name of venue
 //venue location
 //date of event
 function concertThis(arg2) {
-    axios.get("https://rest.bandsintown.com/artists/" + arg2 + "/events?app_id=codingbootcamp").then(function(response) {
+    axios.get("https://rest.bandsintown.com/artists/" + arg2 + "/events?app_id=codingbootcamp").then(function (response) {
         console.log(response.data[0].venue.name)
         console.log(response.data[0].venue.city + ", " + response.data[0].venue.country)
         console.log(moment(response.data[0].datetime).format("MM-DD-YYYY"))
+    }
+    )
 }
-    )}
 
 //spotify -- spotify-this-song function//
 //artist-s
@@ -49,45 +57,45 @@ function concertThis(arg2) {
 //preview link of song from spotify
 //album song is from
 function spotifyThisSong(arg2) {
-    spotify.search({ type: 'track', query: arg2 }).then(function(response){
-        var data = response.tracks.items[0];
+    spotify.search({ type: 'track', query: arg2 }).then(function (response) {
+        var dataOne = response.tracks.items[0];
         console.log(
-            "Artist: " + data.artists[0].name,
-            "\nSong: " + data.name,
-            "\nLink to Song: " + data.preview_url,
-            "\nAlbum: " + data.album.name 
-        );       
+            "Artist: " + dataOne.artists[0].name,
+            "\nSong: " + dataOne.name,
+            "\nLink to Song: " + dataOne.preview_url,
+            "\nAlbum: " + dataOne.album.name
+        );
     });
-    
+
 };
 //random.txt -- do-what-it-says//
-function doWhatItSays(){
-    
-var fs = require("fs")
+function doWhatItSays() {
 
-fs.readFile("random.txt", "utf8", function(error, data) {
-  if (error) {
-    return console.log(error)
-  }
- console.log(spotifyThisSong(data))
+    var fs = require("fs")
 
-});
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error)
+        }
+        console.log(spotifyThisSong(data))
+
+    });
 };
 
-switch(arg1) {
+switch (arg1) {
     case "movie-this":
-      movieThis(arg2)
-      break;
+        movieThis(arg2)
+        break;
 
     case "concert-this":
-      concertThis(arg2)
-      break;
-    
+        concertThis(arg2)
+        break;
+
     case "spotify-this-song":
-      spotifyThisSong(arg2)
-      break;
+        spotifyThisSong(arg2)
+        break;
 
     case "do-what-it-says":
-      doWhatItSays()
-      break;
-  };
+        doWhatItSays()
+        break;
+};
